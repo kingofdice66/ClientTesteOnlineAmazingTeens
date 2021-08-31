c6e18a14cd053abb3768f8911a1772f7f94e73fc
unknown <kingofdice66@gmail.com>
2021-08-31 04:11:39 +0300

    reimplemented using Laravel (FIXED ERRORS)(2)

--- resources/src_old/TESTING/Testing.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-function Testing(): JSX.Element {
-    return <div>HELLO WORLD!!</div>;
-}
-
-ReactDOM.render(<Testing />, document.getElementById("test"));
--- resources/src_old/apiURL/ApiURL.tsx
+++
@@ -1,5 +0,0 @@
-// For use with api Laravel
-const apiURL =
-  "http://localhost/projects/ClientTesteOnlineAmazingTeens/website/laravel/public/api/";
-
-export default apiURL;
