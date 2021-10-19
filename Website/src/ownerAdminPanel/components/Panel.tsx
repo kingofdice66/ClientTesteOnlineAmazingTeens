7470b15a0cfd2377198ea68d34ee457c6a8b28d5
kingofdice66 <kingofdice66@gmail.com>
2021-10-19 04:53:35 +0300

    making owner admin panel

--- Website/src/ownerAdminPanel/components/Panel.tsx
+++ Website/src/ownerAdminPanel/components/Panel.tsx
@@ -1,53 +1,23 @@
-import React, { useState, useEffect } from "react";
-import Link from "../../customComponents/Link/Link";
-import URLSearchParameters from "../../customComponents/URLSearchParameters/URLSearchParameters";
+import React from "react";
+import Links from "./Links";
 import Subjects from "./Subjects";
 import Courses from "./Courses";
 
 // ##############################################################
 // #########              Get URL parameters            #########
 // ##############################################################
-// const url: string = window.location.search;
-// const searchParams: URLSearchParams = new URLSearchParams(url);
-const subjectID: number = parseInt(URLSearchParameters.get("subjectID"), 10);
+const url: string = window.location.search;
+const searchParams: URLSearchParams = new URLSearchParams(url);
+const subjects: string = searchParams.get("subjects");
+const subjectID: string = searchParams.get("subjectID");
 // ##############################################################
 
 function Panel(): JSX.Element {
-  const [page, setPage] = useState<string>("");
-
-  const popstate = (): void => {
-    setPage(URLSearchParameters.get("highlight"));
-  };
-
-  useEffect(() => {
-    /** Must use 'popstate' to reload page otherwise components selection won't work as intended. */
-    window.addEventListener("popstate", () => {
-      window.location.reload();
-    });
-    popstate();
-  }, []);
-
   return (
     <>
-      <div style={{ color: "white" }}>
-        <Link
-          to="?highlight=subjects"
-          name="CURSURI"
-          highlight="subjects"
-          setPage={setPage}
-          tabIndex={0}
-        />
-      </div>
-      <Link
-        to="?highlight=courses"
-        name="ADMINISTRATORII CURSULUI"
-        highlight="NULL"
-        setPage={setPage}
-        tabIndex={-1}
-      />
-
-      {page === "subjects" ? <Subjects setPage={setPage} /> : ""}
-      {page === "courses" ? <Courses /> : ""}
+      <Links />
+      {subjects ? <Subjects /> : ""}
+      {subjectID ? <Courses /> : ""}
     </>
   );
 }
