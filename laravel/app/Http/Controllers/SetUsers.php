56c84b8dc40f83b18593eb6d6bdaf167ec6c8bd0
kingofdice66 <kingofdice66@gmail.com>
2021-11-22 02:22:02 +0200

    setting up registration

--- laravel/app/Http/Controllers/SetUsers.php
+++ laravel/app/Http/Controllers/SetUsers.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 use App\Helpers\CustomFunctions;
 use Carbon\Carbon;
 
@@ -13,7 +14,10 @@ class SetUsers extends Controller
     private $data = NULL;
     private $username = NULL;
     private $email = NULL;
+    private $password = NULL;
+    private $hashedPassword = NULL;
     private $token = NULL;
+    private $dateTime = NULL;
     private $expiration = NULL;
 
     public function __construct()
@@ -21,26 +25,23 @@ class SetUsers extends Controller
         $this->data = (new CustomFunctions)->jsonDecode();
         $this->username = $this->data["username"];
         $this->email = $this->data["email"];
+        $this->password = $this->data["password"];
+        $this->hashedPassword = Hash::make($this->password);
         $this->token = Str::random(40); // Generates cryptographically secure pseudo-random bytes. Uses PHP 'random_bytes'
+        $this->dateTime = (new Carbon)->format((new CustomFunctions)->dateTimeFormat());
         $this->expiration = (new Carbon)->addMinutes(30)->format((new CustomFunctions)->dateTimeFormat()); // Token expires after 30 minutes.
     }
 
     public function setData()
     {
-        // DB::table("users")
-        //     ->insert([
-        //         "username" => $this->username,
-        //         "email" => $this->email,
-        //         "token" => $this->token,
-        //         "expiration" => $this->expiration,
-        //     ]);
-
-        return [
-            "SetUsers" => "success",
-            "username" => $this->username,
-            "email" => $this->email,
-            "token" => $this->token,
-            "expiration" => $this->expiration,
-        ];
+        DB::table("users")
+            ->insert([
+                "username" => $this->username,
+                "email" => $this->email,
+                "password" => $this->hashedPassword,
+                "token" => $this->token,
+                "created_at" => $this->dateTime,
+                "expiration_token" => $this->expiration,
+            ]);
     }
 }
--- nextjs/components/Registration/Registration.tsx
+++ nextjs/components/Registration/Registration.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import axios from "axios";
+import apiURL from "../ApiURL/ApiURL";
 
 interface IUserInfo {
   username: string;
   password: string;
+  retypePassword: string;
   email: string;
 }
 
@@ -11,33 +13,71 @@ function Registration(): JSX.Element {
   const [userInfo, setUserInfo] = useState<IUserInfo>({
     username: "",
     password: "",
+    retypePassword: "",
     email: "",
   });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
 
+    console.log("username: ", userInfo.username);
+    console.log("password: ", userInfo.password);
+    console.log("retypePassword: ", userInfo.retypePassword);
+    console.log("email: ", userInfo.email);
     console.log("form submitted");
+
+    const data = {
+      username: userInfo.username,
+      password: userInfo.password,
+      email: userInfo.email,
+    };
+
+    console.log("data: ", data);
+
+    axios
+      .post(`${apiURL}/setUsers`, data)
+      .then((res: any) => console.log("res: ", res.data))
+      .catch((err: any) => console.error(err));
   };
 
   return (
     <form onSubmit={(e): void => handleSubmit(e)}>
+      {/* prettier-ignore */}
       <label htmlFor="username">
         Nume utilizator: <br />
-        <input id="username" type="text" placeholder="username..." />
+        <input
+          id="username"
+          type="text"
+          placeholder="username..."
+          value={userInfo.username}
+          onChange={(e): void => setUserInfo({ ...userInfo, username: e.target.value })}
+        />
       </label>
       <br />
 
+      {/* prettier-ignore */}
       <label htmlFor="email">
         EMail: <br />
-        <input id="email" type="email" placeholder="adresa de email..." />
+        <input 
+          id="email" 
+          type="email" 
+          placeholder="adresa de email..." 
+          value={userInfo.email}
+          onChange={(e): void => setUserInfo({ ...userInfo, email: e.target.value })}
+        />
       </label>
       <br />
-      
 
+      {/* prettier-ignore */}
       <label htmlFor="password">
         Introdu parola: <br />
-        <input id="password" type="password" placeholder="introdu parola..." />
+        <input 
+          id="password" 
+          type="password" 
+          placeholder="introdu parola..." 
+          value={userInfo.password}
+          onChange={(e): void => setUserInfo({ ...userInfo, password: e.target.value })}
+        />
       </label>
       <br />
 
@@ -48,6 +88,8 @@ function Registration(): JSX.Element {
           id="retypePassword"
           type="password"
           placeholder="reintrodu parola..."
+          value={userInfo.retypePassword}
+          onChange={(e): void => setUserInfo({ ...userInfo, retypePassword: e.target.value })}
         />
       </label>
       <br />
